9ffc544f67ad7b67c7ade0cc0819af56b7b3223a
Anurag <anuragmourya975@gmail.com>
2022-11-25 23:28:33 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Banner } from "./Banner";
+import { Footer } from "./Footer";
 import { NavBar } from "./NavBar";
+import { Profile } from "./Profile";
 
 function Home() {
   return (
     <div>
       <NavBar />
       <Banner />
+      <Profile />
+      <Footer />
     </div>
   );
 }
